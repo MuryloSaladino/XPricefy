ea94ee9005e413e4113fd19dcdcc03eeea9a29c8
MuryloSaladino <murylosaladino@gmail.com>
2025-02-19 16:22:21 -0300

    feat: extension method to configure application rules

---
+++ Server/Server.Application/ServiceExtensions.cs
@@ -0,0 +1,57 @@
+using System.Reflection;
+using System.Text;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+using Server.Application.Common.Behaviors;
+using Server.Application.Common.Session;
+using Server.Application.Config;
+using Server.Application.Services.Authentication;
+using Server.Domain.Contracts;
+using Server.Domain.Entities;
+
+namespace Server.Application;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        services.AddScoped<PasswordHasher<User>>();
+        services.AddScoped<UserSession>();
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+    }
+
+    public static void ConfigureAuthentication(this IServiceCollection services)
+    {
+        services.AddCors();
+        services.AddControllers();
+
+        var key = Encoding.ASCII.GetBytes(DotEnv.Get("SECRET_KEY"));
+
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = false;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+    }
+}
