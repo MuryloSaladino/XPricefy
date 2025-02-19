02c3b9617d6da0eea5fccdf0fb24372143c87abd
MuryloSaladino <murylosaladino@gmail.com>
2025-02-19 16:23:47 -0300

    feat: authentication service implementation

---
+++ Server/Server.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,35 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Server.Application.Config;
+using Server.Domain.Contracts;
+
+namespace Server.Application.Services.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    public string SecretKey { get; private set; } = DotEnv.Get("SECRET_KEY");
+    public int ExpireHours { get; private set; } = int.Parse(DotEnv.Get("EXPIRE_HOURS"));
+
+    public string GenerateUserToken(string userId)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(SecretKey);
+        var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity([ new Claim("userId", userId) ]),
+            
+            Expires = DateTime.UtcNow.AddHours(ExpireHours),
+            
+            SigningCredentials = new(
+                new SymmetricSecurityKey(key), 
+                SecurityAlgorithms.HmacSha256Signature
+            ),
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
+
