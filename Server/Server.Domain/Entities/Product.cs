e839ce6dbabdc06dc59c767907a2fc8492b2286d
MuryloSaladino <murylosaladino@gmail.com>
2025-02-19 13:59:58 -0300

    feat: product entity

---
+++ Server/Server.Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+using Server.Domain.Common;
+
+namespace Server.Domain.Entities;
+
+public class Product : BaseEntity
+{
+    public required string Name { get; set; }
+    public required float AnnualPrice { get; set; }
+    public required int ClientsNumber { get; set; }
+    public required int YearsToPay { get; set; }
+}
