c40400971c99e38f56acc8ac09375a55aa590cf3
MuryloSaladino <murylosaladino@gmail.com>
2025-02-19 14:53:00 -0300

    refact: inherit from base entity

--- Server/Server.Domain/Entities/ProductHistory.cs
+++ Server/Server.Domain/Entities/ProductHistory.cs
@@ -1,12 +1,11 @@
+using Server.Domain.Common;
 using Server.Domain.Enums;
 
 namespace Server.Domain.Entities;
 
-public class ProductHistory
+public class ProductHistory : BaseEntity
 {
-    public Guid Id { get; } = Guid.NewGuid();
     public required HistoryAction Action;
-    public required DateTime Date = DateTime.Now;
     public string? Description;
     public required User User;
     public required Product Product;
