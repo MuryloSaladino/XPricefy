99d482217928d1f2ea5acc1d35eeb81b646870bc
MuryloSaladino <murylosaladino@gmail.com>
2025-02-19 16:24:17 -0300

    feat: IsAdmin attribute for future authorization

--- Server/Server.Domain/Entities/User.cs
+++ Server/Server.Domain/Entities/User.cs
@@ -6,4 +6,5 @@ public class User : BaseEntity
 {
     public required string Username { get; set; }
     public required string Password { get; set; }
+    public bool IsAdmin { get; set; } = false;
 }
