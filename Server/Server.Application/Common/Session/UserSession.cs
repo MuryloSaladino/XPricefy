d7f2769edc7acdd8ba4b97078e280363268b4659
MuryloSaladino <murylosaladino@gmail.com>
2025-02-19 16:22:34 -0300

    feat: user session attributes

--- Server/Server.Application/Common/Session/UserSession.cs
+++ Server/Server.Application/Common/Session/UserSession.cs
@@ -2,5 +2,6 @@ namespace Server.Application.Common.Session;
 
 public class UserSession
 {
-    public string? Username { get; set; } = null!;
+    public string? Username { get; set; } = null;
+    public int? UserId { get; set; } = null;
 }
