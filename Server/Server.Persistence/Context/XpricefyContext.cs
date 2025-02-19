eb668e2fa8322e1064e905018b802323ec699ae9
MuryloSaladino <murylosaladino@gmail.com>
2025-02-19 14:54:02 -0300

    feat: database context

---
+++ Server/Server.Persistence/Context/XpricefyContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Server.Domain.Entities;
+
+namespace Server.Persistence.Context;
+
+public class XpricefyContext(DbContextOptions<XpricefyContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductHistory> ProductHistories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .HasKey(u => u.Id);
+        
+        modelBuilder.Entity<Product>()
+            .HasKey(s => s.Id);
+
+        modelBuilder.Entity<ProductHistory>()
+            .HasKey(s => s.Id);
+    }
+}
