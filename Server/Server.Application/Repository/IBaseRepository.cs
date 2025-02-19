749c2e53431770e11f13d82ea7fee82faa29142c
MuryloSaladino <murylosaladino@gmail.com>
2025-02-19 14:22:41 -0300

    feat: base repository

---
+++ Server/Server.Application/Repository/IBaseRepository.cs
@@ -0,0 +1,13 @@
+using Server.Domain.Common;
+
+namespace Server.Application.Repository;
+
+public interface IBaseRepository<TEntity>
+    where TEntity : BaseEntity
+{
+    void Create(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    Task<TEntity?> Get(Guid id, CancellationToken cancellationToken);
+    Task<List<TEntity>> GetAll(CancellationToken cancellationToken);
+}
