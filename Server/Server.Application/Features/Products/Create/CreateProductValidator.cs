b24f3b05704e50ba30e4bcef7b3020d2d9313ed6
MuryloSaladino <murylosaladino@gmail.com>
2025-02-20 13:04:56 -0300

    refact: validation rules for numbers

--- Server/Server.Application/Features/Products/Create/CreateProductValidator.cs
+++ Server/Server.Application/Features/Products/Create/CreateProductValidator.cs
@@ -7,8 +7,8 @@ public class CreateProductValidator : AbstractValidator<CreateProductRequest>
     public CreateProductValidator()
     {
         RuleFor(p => p.Name).MinimumLength(3).MaximumLength(25);
-        RuleFor(p => p.AnnualPrice).Must(p => float.Round(p, 2) == 0);
+        RuleFor(p => p.AnnualPrice).GreaterThan(0);
         RuleFor(p => p.ClientsNumber).GreaterThan(0);
-        RuleFor(p => p.YearsToPay).GreaterThan(0);
+        RuleFor(p => p.YearsToPay).GreaterThan(0).LessThan(6);
     }
 }
