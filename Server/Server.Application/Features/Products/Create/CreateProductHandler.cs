c8005955ece1b1e91dbed0c7a26c59b1ba750577
MuryloSaladino <murylosaladino@gmail.com>
2025-02-20 20:07:12 -0300

    feat: setting history for delete

--- Server/Server.Application/Features/Products/Create/CreateProductHandler.cs
+++ Server/Server.Application/Features/Products/Create/CreateProductHandler.cs
@@ -1,19 +1,29 @@
 using AutoMapper;
 using MediatR;
+using Server.Application.Common.Exceptions;
 using Server.Application.Repository;
 using Server.Application.Repository.Products;
+using Server.Application.Repository.Users;
+using Server.Domain.Common;
 using Server.Domain.Entities;
+using Server.Domain.Enums;
 
 namespace Server.Application.Features.Products.Create;
 
 public sealed class CreateProductHandler(
+    IProductHistoryRepository productHistoryRepository,
     IProductRepository productRepository,
+    IUserRepository userRepository,
     IUnitOfWork unitOfWork,
+    UserSession session,
     IMapper mapper
 ) : IRequestHandler<CreateProductRequest, CreateProductResponse>
 {
+    private readonly IProductHistoryRepository productHistoryRepository = productHistoryRepository;
     private readonly IProductRepository productRepository = productRepository;
+    private readonly IUserRepository userRepository = userRepository;
     private readonly IUnitOfWork unitOfWork = unitOfWork;
+    private readonly UserSession session = session;
     private readonly IMapper mapper = mapper;
 
     public async Task<CreateProductResponse> Handle(
@@ -21,6 +31,18 @@ public sealed class CreateProductHandler(
     {
         var product = mapper.Map<Product>(request);
         productRepository.Create(product);
+        
+        var user = await userRepository.Get(session.Id, cancellationToken)
+            ?? throw new AppException("User not found", 404);
+
+        var history = new ProductHistory()
+        {
+            Action = HistoryAction.CREATE,
+            Product = product,
+            User = user
+        };
+        productHistoryRepository.Create(history);
+
         await unitOfWork.Save(cancellationToken);
 
         return mapper.Map<CreateProductResponse>(product);
