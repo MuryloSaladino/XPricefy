81c4b6241e72f98b0d91fbb29d32757dd3d6a3fa
MuryloSaladino <murylosaladino@gmail.com>
2025-02-20 23:24:44 -0300

    feat: nullable id

--- Server/Server.Domain/Common/UserSession.cs
+++ Server/Server.Domain/Common/UserSession.cs
@@ -1,7 +1,7 @@
 namespace Server.Domain.Common;
 
-public class UserSession(string username, string id)
+public class UserSession(string username, string? id)
 {
     public string Username { get; set; } = username;
-    public Guid Id { get; set; } = Guid.Parse(id);
+    public Guid? Id { get; set; } = Guid.TryParse(id, out var parsedId) ? parsedId : null;
 }
