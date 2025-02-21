5d724902adfb62feacb9c4e00c824e613c4cdf60
MuryloSaladino <murylosaladino@gmail.com>
2025-02-21 18:30:38 -0300

    checkpoint

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
     selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'frontend';
+
+    constructor(
+        private readonly authService: AuthService
+    ) {
+        console.log(authService);
+        
+    }
 }
