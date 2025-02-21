c1c57b5febe4300b68761810360d700876afa5b8
MuryloSaladino <murylosaladino@gmail.com>
2025-02-20 23:25:01 -0300

    fix: tracking for foreign key

--- Server/Server.Domain/Entities/ProductHistory.cs
+++ Server/Server.Domain/Entities/ProductHistory.cs
@@ -7,6 +7,10 @@ public class ProductHistory : BaseEntity
 {
     public required HistoryAction Action;
     public string? Description;
+
+    public required Guid UserId { get; set; }
     public required User User;
+    
+    public required Guid ProductId { get; set; }
     public required Product Product;
 }
