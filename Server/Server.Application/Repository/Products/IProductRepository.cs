9949adf87a8fe9fc2fedc8cd3e068cb495abd318
MuryloSaladino <murylosaladino@gmail.com>
2025-02-20 23:24:31 -0300

    feat: query including history method

--- Server/Server.Application/Repository/Products/IProductRepository.cs
+++ Server/Server.Application/Repository/Products/IProductRepository.cs
@@ -3,4 +3,6 @@ using Server.Domain.Entities;
 namespace Server.Application.Repository.Products;
 
 public interface IProductRepository : IBaseRepository<Product>
-{ }
+{
+    Task<Product?> GetProductWithHistory(Guid id, CancellationToken cancellationToken);
+}
--- Server/Server.Persistence/Repository/Products/ProductRepository.cs
+++ Server/Server.Persistence/Repository/Products/ProductRepository.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using Server.Application.Repository.Products;
 using Server.Domain.Entities;
 using Server.Persistence.Context;
 
 namespace Server.Persistence.Repository.Products;
 
-public class ProductRepository(XpricefyContext xpricefyContext) 
+public class ProductRepository(XpricefyContext xpricefyContext)
     : BaseRepository<Product>(xpricefyContext), IProductRepository
-{ }
+{
+    public async Task<Product?> GetProductWithHistory(Guid id, CancellationToken cancellationToken)
+        => await context
+            .Set<Product>()
+            .Include(p => p.ProductHistories)
+            .FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
+}
