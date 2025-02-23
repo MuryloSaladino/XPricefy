03d8d5ec14ee83fc3438b75be638e2bb45e9bd3e
MuryloSaladino <murylosaladino@gmail.com>
2025-02-23 14:39:57 -0300

    feat: pricing calculations service

---
+++ frontend/src/app/core/services/pricing.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { Product, ProductSummary } from "../types/product.types";
+
+const MARGIN = 0.05;
+const EURO_PRICE = 5.75;
+
+@Injectable({
+    providedIn: "root"
+})
+export class PricingService {
+
+    getCostWithMargin(product: Product | ProductSummary) {
+        return product.annualPrice * (1 + MARGIN);
+    }
+
+    getAnnualCost(product: Product) {
+        return product.annualPrice / product.yearsToPay;
+    }
+
+    getCostPerClient(product: Product) {
+        return this.getAnnualCost(product) / product.clientsNumber;
+    }
+
+    getCostPerClientWithTax(product: Product) {
+        return this.getCostPerClient(product) * (1 + MARGIN);
+    }
+
+    getCostPerClientWithEuroTax(product: Product) {
+        return this.getCostPerClientWithTax(product) / EURO_PRICE;
+    }
+
+    getMensalCostPerClientWithEuroTax(product: Product) {
+        return this.getAnnualCost(product) / 12 / product.clientsNumber * (1 + MARGIN) / EURO_PRICE;
+    }
+}
