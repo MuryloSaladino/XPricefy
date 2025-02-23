35335cd573225495e913644135e1fef6a8f36abd
MuryloSaladino <murylosaladino@gmail.com>
2025-02-22 23:56:43 -0300

    feat: routes config

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './pages/login/login.page';
+import { HomePage } from './pages/home/home.page';
+import { authGuard } from './core/guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "login",
+        component: LoginPage,
+    },
+    {
+        path: "",
+        component: HomePage,
+        canActivate: [authGuard]
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
