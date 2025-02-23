a005afbb6b5e17f60feae0a828781b81d87e3a1a
MuryloSaladino <murylosaladino@gmail.com>
2025-02-23 14:40:32 -0300

    feat: home page data logic

--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,10 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ProductsRepository } from "src/app/core/repositories/products.repository";
+import { ProductSummary } from "src/app/core/types/product.types";
 
 @Component({
     selector: "app-home",
     templateUrl: "./home.page.html",
-    styleUrls: ["home.page.css"]
+    styleUrls: ["home.page.css"],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
+    products!: ProductSummary[];
+    openModal: boolean = false;
+
+    constructor(
+        private readonly productsRepository: ProductsRepository,
+    ) {}
+
+    async ngOnInit() {
+        await this.handleUpdateProducts();
+    }
+    
+    async handleUpdateProducts() {
+        this.products = await this.productsRepository.findAllProducts();
+    }
+
+    handleOpenModal() {
+        this.openModal = true;
+    }
+    handleCloseModal() {
+        this.openModal = false;
+    }
 }
