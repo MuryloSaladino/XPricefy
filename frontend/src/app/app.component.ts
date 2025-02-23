55477bcf10e1c6728edb54e0e3b9ddac12949131
MuryloSaladino <murylosaladino@gmail.com>
2025-02-22 23:57:07 -0300

    refact: cleaner root component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,9 @@
 import { Component } from '@angular/core';
-import { AuthService } from './core/services/auth.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
     title = 'frontend';
-
-    constructor(
-        private readonly authService: AuthService
-    ) {
-        console.log(authService);
-        
-    }
 }
