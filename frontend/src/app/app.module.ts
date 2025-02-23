e8cfb4b5d9f86d539b3e4b36f811263a8d3bbbbe
MuryloSaladino <murylosaladino@gmail.com>
2025-02-23 14:38:54 -0300

    add: module dependencies

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,24 +8,40 @@ import { LoginPage } from './pages/login/login.page';
 import { HomePage } from './pages/home/home.page';
 import { TokenHeaderInterceptor } from './core/interceptors/token-header.interceptor';
 import { ButtonComponent } from './shared/button/button.component';
-import ModalComponent from './shared/modal/modal.component';
 import { IconComponent } from './shared/icon/icon.component';
 import { InputComponent } from './shared/input/input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductTableComponent } from './pages/home/components/products-table/product-table.component';
+import { BrlCurrencyPipe } from './core/pipes/brl-currency.pipe';
+import { FormatDatePipe } from './core/pipes/format-date.pipe';
+import { HeaderComponent } from './shared/header/header.component';
+import { CreateProductModal } from './pages/home/components/create-product-modal/create-product-modal.component';
+import { AvatarComponent } from './shared/avatar/avatar.component';
+import { PricingDetails } from './pages/pricing-details/pricing-details.page';
 
 @NgModule({
     declarations: [
         AppComponent,
         
         // Pages
-        LoginPage,
         HomePage,
+        LoginPage,
+        PricingDetails,
 
         // Shared Components
+        AvatarComponent,
         ButtonComponent,
-        ModalComponent,
+        HeaderComponent,
         IconComponent,
         InputComponent,
+
+        // Page Components
+        CreateProductModal,
+        ProductTableComponent,
+
+        // Pipes
+        BrlCurrencyPipe,
+        FormatDatePipe,
     ],
     imports: [
         BrowserModule,
