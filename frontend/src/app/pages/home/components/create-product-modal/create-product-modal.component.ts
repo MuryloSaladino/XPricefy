ef52a3a7ff6c1511f9176e8d4c94aca69a504a85
MuryloSaladino <murylosaladino@gmail.com>
2025-02-23 14:41:07 -0300

    feat:L modal component for creating products

---
+++ frontend/src/app/pages/home/components/create-product-modal/create-product-modal.component.ts
@@ -0,0 +1,60 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ProductsRepository } from "src/app/core/repositories/products.repository";
+
+@Component({
+    selector: "create-product-modal",
+    templateUrl: "./create-product-modal.component.html",
+    styleUrls: ["./create-product-modal.component.css"]
+})
+export class CreateProductModal implements OnInit {
+
+    createProductForm!: FormGroup;
+
+    @Input() productId!: string | null;
+
+    @Output() onClose = new EventEmitter<void>();
+    @Output() onCreation = new EventEmitter<void>();
+
+
+    constructor(
+        private readonly formBuilder: FormBuilder,
+        private readonly productsRepository: ProductsRepository,
+    ) {}
+
+    ngOnInit(): void {
+        this.createProductForm = this.formBuilder.group({
+            name: [null, [Validators.required, Validators.minLength(3)]],
+            annualPrice: [null, [Validators.required, Validators.min(0)]],
+            clientsNumber: [null, [Validators.required, Validators.min(0)]],
+            yearsToPay: [null, [Validators.required, Validators.min(0), Validators.max(5)]],
+        })
+    }
+
+    closeModal() {
+        this.onClose.emit();
+    }
+
+    async submit() {
+        console.log(this.createProductForm.value);
+        
+        try {
+            const name = this.createProductForm.value.name;
+            const annualPrice = Number(this.createProductForm.value.annualPrice);
+            const clientsNumber = Number(this.createProductForm.value.clientsNumber);
+            const yearsToPay = Number(this.createProductForm.value.yearsToPay);
+
+            await this.productsRepository.createProduct({
+                name,
+                annualPrice,
+                clientsNumber,
+                yearsToPay,
+            });
+            
+            this.closeModal();
+            this.onCreation.emit();
+        } catch {
+            
+        }
+    }
+}
