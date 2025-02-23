d2e7eb083858ed933d1b2ab36229713eb3981451
MuryloSaladino <murylosaladino@gmail.com>
2025-02-22 23:57:20 -0300

    feat: core modules and pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,43 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { HomePage } from './pages/home/home.page';
+import { TokenHeaderInterceptor } from './core/interceptors/token-header.interceptor';
+import { ButtonComponent } from './shared/button/button.component';
+import ModalComponent from './shared/modal/modal.component';
+import { IconComponent } from './shared/icon/icon.component';
+import { InputComponent } from './shared/input/input.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
         AppComponent,
+        
+        // Pages
+        LoginPage,
+        HomePage,
+
+        // Shared Components
+        ButtonComponent,
+        ModalComponent,
+        IconComponent,
+        InputComponent,
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,  
     ],
-    providers: [],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: TokenHeaderInterceptor,
+            multi: true
+        },
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
