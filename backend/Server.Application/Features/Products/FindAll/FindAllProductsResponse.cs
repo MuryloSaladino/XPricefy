5a790971cf3fd6d93a80a5318252986134962928
MuryloSaladino <murylosaladino@gmail.com>
2025-02-24 15:43:40 -0300

    refact: more info on products findall response

--- backend/Server.Application/Features/Products/FindAll/FindAllProductsResponse.cs
+++ backend/Server.Application/Features/Products/FindAll/FindAllProductsResponse.cs
@@ -2,7 +2,11 @@ namespace Server.Application.Features.Products.FindAll;
 
 public sealed record FindAllProductsResponse(
     string Id,
+    DateTime CreatedAt,
     DateTime? UpdatedAt,
+    DateTime? DeletedAt,
     string Name,
-    decimal Price
+    decimal Price,
+    int ClientsNumber,
+    int YearsToPay
 );
