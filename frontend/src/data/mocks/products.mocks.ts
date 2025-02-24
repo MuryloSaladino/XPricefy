9c3c8a15a76f0fb60cda60549980fb790ae90639
MuryloSaladino <murylosaladino@gmail.com>
2025-02-24 14:27:04 -0300

    feat: data mocks for population

---
+++ frontend/src/data/mocks/products.mocks.ts
@@ -0,0 +1,34 @@
+import { ProductCreation } from "src/app/core/types/product.types";
+
+export const productMocks: ProductCreation[] = [
+    {
+        name: "Xpertify",
+        price: 23415.87,
+        clientsNumber: 7,
+        yearsToPay: 3
+    },
+    {
+        name: "Dataxtract",
+        price: 12637.87,
+        clientsNumber: 3,
+        yearsToPay: 4
+    },
+    {
+        name: "Datafox",
+        price: 187026.32,
+        clientsNumber: 30,
+        yearsToPay: 5
+    },
+    {
+        name: "XTower",
+        price: 5233.63,
+        clientsNumber: 2,
+        yearsToPay: 4
+    },
+    {
+        name: "ICOnnect",
+        price: 371623.54,
+        clientsNumber: 23,
+        yearsToPay: 5
+    },
+]
---
+++ frontend/src/data/mocks/users.mocks.ts
@@ -0,0 +1,9 @@
+export const usernameMocks: string[] = [
+    "olv2ct",
+    "sau8ct",
+    "feh1de",
+    "enb1jvl",
+    "god2ct",
+]
+
+export const defaultPassword: string = "12345678";
